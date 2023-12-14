5222c281ab83f67ee4c694aa7d8385cebf15e54b
Ken <Ken@DESKTOP-D072FBA>
2023-12-14 15:49:02 -0500

    added final touches

--- Troubleshootingg/Program.cs
+++ Troubleshootingg/Program.cs
@@ -10,49 +10,72 @@ namespace Troubleshootingg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Case 1");
-            
-            int number = 2;
+            // Console.WriteLine("Case 1");
 
-            if (number > 3)
-            {
-                Console.WriteLine("Talet är större än tre"); // missing ;
-            }
-            else if (number < 3) // else if needs to be separate words
+            // int number = 2; 
 
-                Console.WriteLine("Talet är mindre än tre");
+            // if (number > 3)
+            // {
+            //Console.WriteLine("Talet är större än tre"); // missing ;
+            // }
+            //else if (number < 3) // else if needs to be separate words
 
-            Console.WriteLine("Case 2");
+            //    Console.WriteLine("Talet är mindre än tre");
 
-            for (int i = 1; i < 100; i++) // the condiion needs to be spaced out for = + <
-            {
-                Console.WriteLine(i);
-            }
+            //Console.WriteLine("Case 2");
 
-            Console.WriteLine("Case 3");
+            //for (int i = 1; i < 100; i++) // the condiion needs to be spaced out for = + <
+            //{
+            //    Console.WriteLine(i);
+            //}
 
-            for (int i = 1; i <= 5; i++)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write(j + " "); // so J 1-5 counting up with a space in between
-                }
-                Console.WriteLine(); //every condition skip to next line
-            }
+            //Console.WriteLine("Case 3");
 
-            Console.WriteLine("Case 3");
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j + " "); // so J 1-5 counting up with a space in between
+            //    }
+            //    Console.WriteLine(); //every condition skip to next line/column
+            //}
 
-            int i = 1;
-            while (i <= 5)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write();
-                }
+            //Console.WriteLine("Case 4");
 
-                Console.WriteLine();
-                i++;
-            }
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j); // missing defined value in brackets 
+            //    }
+
+            //    Console.WriteLine();
+            //    i++;
+            //}
+
+            //Console.WriteLine("Case 5");
+
+            //int i = 1;
+
+            //switch (i)
+            //{
+            //    case 1:                 
+            //        break; // added break; to printout nothing.
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Other");
+            //        break;
+            //}
+
+            //int i = 10;
+
+            //if (i = 5) // cannot create an int through booleans need to use == if statement
+            //{
+            //    Console.WriteLine("i är 5"); // runs only if above condition was true.
+            //}
         }
     }
 }
